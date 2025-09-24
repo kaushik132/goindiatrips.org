1f4b0f8d2aadf337119f19e7dd95ce0f8eeaa086
kaushik132 <kaushik.dey.ahit@gmail.com>
2025-09-24 13:10:58 +0530

    new

---
+++ app/Admin/Controllers/GalleryController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use OpenAdmin\Admin\Controllers\AdminController;
+use OpenAdmin\Admin\Form;
+use OpenAdmin\Admin\Grid;
+use OpenAdmin\Admin\Show;
+use \App\Models\Gallery;
+
+class GalleryController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'Gallery';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Gallery());
+
+        $grid->column('id', __('Id'));
+        $grid->column('image', __('Image'))->image('/uploads/', 100, 100);
+
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Gallery::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('image', __('Image'));
+        $show->field('alt', __('Alt'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Gallery());
+
+        $form->image('image', __('Image'));
+        $form->text('alt', __('Alt'));
+
+        return $form;
+    }
+}
